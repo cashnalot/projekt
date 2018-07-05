7e3368dbedbe9ccbbd13ae7f37573badcbc5a816
Piotr Gładysz <piotr.gladysz@outlook.com>
2018-07-06 01:10:36 +0200

    blockchain page update

--- page-blockchain.php
+++ page-blockchain.php
@@ -1,32 +1,31 @@
-<?php
-get_header(); 
+<?php get_header(  ) ?>
+    <div class="row">
+        <div class=" col-lg-1 col-md-1 col-sm-1 col-">
+            <p class="empty-holder"></p>
+        </div>
+        <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col- mainBackground">
+            <?php
+                $query = new WP_Query(array(
+                'category_name'=> 'analizy',
+                
+                ));
 
-?>
-<div class="row">
-    <div class=" col-lg-1 col-md-1 col-sm-1 col-">
+                if ($query->have_posts()):
+                    while ($query->have_posts()):
+                        $query->the_post();
+            ?>
+                        <?php the_title('<h1>','</h1>') ?>
+                        <?php the_content('<p>','</p>') ?>
+                    
+                    <?php endwhile ?>
+                    <?php wp_reset_postdata(); ?>
+                    <?php else: ?>
+                <?php echo "brak analiz"; ?>
+                
+                <?php endif ?>
+        </div>
+        <div class=" col-lg-1 col-md-1 col-sm-1 col-"> 
         <p class="empty-holder"></p>
+        </div>
     </div>
-    <div class="col-xl-10 col-lg-10 col-md-10 col-sm-10 col- mainBackground">
-    
-   
-
-
-        <?php if ( have_posts() ) :
-        while ( have_posts() ) :
-        the_post(); ?>
-        <?php the_post_thumbnail('medium'); ?>
-        <?php the_title('<h1>','</h1>'); ?> 
-                <div class="content">
-                    <?php the_content(); ?>
-                </div>
-        <?php endwhile; ?>
-        <?php else: ?>
-        <?php endif; ?>
-    </div>
-
-    
-    <div class=" col-lg-1 col-md-1 col-sm-1 col-">
-        
-    </div>
-</div>
-<?php get_footer(); ?>
+<?php get_footer( ) ?>
